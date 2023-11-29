7ce0f4445d9e0643eb4c8bdcbf80a632d8267ce0
Rossario Catherine Elfrida <roselfridaa@gmail.com>
2023-11-29 16:07:46 +0800

    sentry.io

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const Sentry = require("@sentry/node")
+const { ProfilingIntegration } = require("@sentry/profiling-node")
 const express = require('express')
 const swaggerJSON = require('./openapi.json')
 const swaggerUI = require('swagger-ui-express')
@@ -5,11 +7,13 @@ const path = require('path')
 const flash = require('express-flash')
 const session = require("express-session")
 const passport = require('./utils/passport')
+const morgan=require('morgan')
 
 const app = express()
 const port = 3000
 const routers = require('./router')
 
+app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.urlencoded({ extended:false })) //req.body untuk form data
 app.use(session({ // config middleware session
@@ -25,8 +29,36 @@ app.set("view engine", "ejs"); // register ejs sebagai view engine flash
 app.set("views", path.join(__dirname, './app/view')) // mengubah folder views ke 
 // app view
 
+Sentry.init({
+  dsn: 'https://48992ad1c3165e10e7c492852002617c@o4506303591874560.ingest.sentry.io/4506303599935488',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON))
 app.use(routers)
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next) {
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 app.listen(port, () => {
   console.log(`Server run at http://localhost:${port}`)
